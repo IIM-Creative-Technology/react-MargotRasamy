241af9327df8ce2a4425d5feab772e1cc134313a
Margot Rasamy <rasamymargot@gmail.com>
2021-02-05 12:15:33 +0100

    [Landing page] Improvement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import './assets/styles/main.scss';
 import LoaderSpinner from './components/LoaderSpinner'
 import LandingPage from './components/landingPage'
+import Header from './components/Header'
+import React, { useRef } from 'react';
 
 function App() {
   const loading = false
+  const app = useRef(null);
   return (
-    <div className="App">
+    <div className="App" ref={app}>
       {/* header to put here */}
-
-      
+      <Header />
       { !!loading
-        ? <LoaderSpinner/>
+        ? <LoaderSpinner />
         : <LandingPage />
       }
 
--- frontend/src/components/landingPage/hero.js
+++ frontend/src/components/landingPage/hero.js
@@ -1,7 +1,12 @@
 function Hero() {
   return (
     <div className="hero">
-      OTAKUKUIZ
+      <div className="hero-caption">
+        <h1>OtaKuiz</h1>
+        <p>Challenge tes connaissances de Otaku avec OtaKuiz. 
+          Crée tes propres Quiz et défie tous tes potes. Fait monter ton score dans le classement des Top 5 Best otakus pour remporter un prix d'une valeur de 800 euros !
+        </p> 
+      </div> 
     </div>
   );
 }
