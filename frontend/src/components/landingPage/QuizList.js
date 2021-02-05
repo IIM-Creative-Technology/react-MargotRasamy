2806ad088fa497307c9e42640f759857fb9b2a89
Margot Rasamy <rasamymargot@gmail.com>
2021-02-05 17:21:03 +0100

    [Scss] style improvement

---
+++ frontend/src/components/landingPage/QuizList.js
@@ -0,0 +1,36 @@
+import QuizCategory from './QuizCategory'
+function QuizList() {
+    const quizDatas = [
+      {
+        title: 'L\'attaque des titans',
+        caption: 'Incollable sur Shingeki no Kyojin ? Tente ta chance !',
+        image: 'shin'
+      },
+      {
+        title: 'Fruits basket',
+        caption: 'Fan des shojo classiques ? Tente ta chance !',
+        image: 'fruits-basket'
+      },
+      {
+        title: 'Code geass',
+        caption: 'Prêt à défier les britanniens ? Yes, my lord !',
+        image: 'code-geass'
+      },
+      {
+        title: 'Death note',
+        caption: 'Réponds aux questions ou ton nom risque d\'y être inscrit !',
+        image: 'death-note'
+      },
+      
+    ]
+
+    return (
+      <div className="quiz-list">
+        { quizDatas &&
+            quizDatas.map((quizData, index) => { return <QuizCategory key={index} quiz={quizData} /> })
+        }
+      </div>
+    );
+  }
+  
+  export default QuizList;
--- frontend/src/components/landingPage/SectionQuiz.js
+++ frontend/src/components/landingPage/SectionQuiz.js
@@ -1,34 +1,9 @@
-import QuizCategory from './QuizCategory'
-function SectionQuiz() {
-    const quizDatas = [
-      {
-        title: 'L\'attaque des titans',
-        caption: 'Incollable sur Shingeki no Kyojin ? Tente ta chance !',
-        image: 'shin'
-      },
-      {
-        title: 'Fruits basket',
-        caption: 'Fan des shojo classiques ? Tente ta chance !',
-        image: 'fruits-basket'
-      },
-      {
-        title: 'Code geass',
-        caption: 'Prêt à défier les britanniens ? Yes, my lord !',
-        image: 'code-geass'
-      },
-      {
-        title: 'Death note',
-        caption: 'Réponds aux questions ou ton nom risque d\'y être inscrit !',
-        image: 'death-note'
-      },
-      
-    ]
+import QuizList from './QuizList'
 
+function SectionQuiz() {
     return (
       <div className="section-quiz">
-        { quizDatas &&
-            quizDatas.map((quizData, index) => { return <QuizCategory key={index} quiz={quizData} /> })
-        }
+        <QuizList />
       </div>
     );
   }
