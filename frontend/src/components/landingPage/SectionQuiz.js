3aeb3a507d8fa14b7cf51b173829933c2e25d696
Margot Rasamy <rasamymargot@gmail.com>
2021-02-05 13:42:46 +0100

    [Landing page] Style

---
+++ frontend/src/components/landingPage/SectionQuiz.js
@@ -0,0 +1,14 @@
+function Hero() {
+    return (
+      <div className="hero">
+        <div className="hero-caption">
+          <h1>OtaKuiz</h1>
+          <p>Challenge tes connaissances de Otaku avec OtaKuiz. 
+            Crée tes propres Quiz et défie tous tes potes. Fait monter ton score dans le classement des Top 5 Best otakus pour remporter un prix d'une valeur de 800 euros !
+          </p> 
+        </div> 
+      </div>
+    );
+  }
+  
+  export default Hero;
