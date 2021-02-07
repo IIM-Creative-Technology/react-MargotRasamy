c9524c6d5a81bd07417e598d3aadcefe34fe47d3
Margot Rasamy <rasamymargot@gmail.com>
2021-02-07 16:18:26 +0100

    [404] Added 404 page when route doesn't exist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import './assets/styles/main.scss';
+import './assets/styles/main.scss'
 import LandingPage from './components/landingPage/LandingPage'
 import QuizPage from './components/quizPage/QuizPage'
 import Header from './components/Header'
-import React, { useRef } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useRef } from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+import Error404 from './components/Error404'
 
 function App() {
   const app = useRef(null);
@@ -15,6 +16,8 @@ function App() {
           <Route exact path="/challenge-expert" component={LandingPage} />
           <Route path="/quiz/:manga" component={QuizPage} />
           <Route exact path="/" component={LandingPage} />
+          <Route path='/404' exact={true} component={Error404} />
+          <Redirect from='*' exact={true} to="/404" />
         </Switch>
       </div>
     </BrowserRouter>
--- frontend/src/components/Error404.js
+++ frontend/src/components/Error404.js
@@ -1,7 +1,8 @@
 function Error404() {
     return (
         <div className="error-page">
-            
+            <h1>Error 404</h1>
+            <h4>Désolé ! Cette page n'existe pas ou est en cours de maintenance. Veuillez réessayer plus tard.</h4>
         </div>
     );
 }
